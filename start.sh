391a9fcc0be363cf3a4ce9fd2d2bdee5679a2f44
ABDULLAH <166320271+mdabdullahsk@users.noreply.github.com>
2026-06-28 07:02:37 +0100

    Remove commented out pulseaudio line

--- start.sh
+++ start.sh
@@ -3,10 +3,6 @@
 # DBUS সার্ভিস চালু করা
 service dbus start
 
-# সাউন্ডের প্রয়োজন না থাকলে নিচের লাইনটির শুরুতে '#' দিয়ে বন্ধ করে দিতে পারেন। 
-# এটি সার্ভারের প্রসেসরের ওপর চাপ অনেক কমাবে।
-pulseaudio --start --system --disallow-exit --disable-shm --realtime=no
-
 # XRDP সার্ভিস চালু করা
 service xrdp start
 
